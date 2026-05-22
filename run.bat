c66fb92a5e6d3f6a1f08219fdaa1d12d5124e1ce
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2026-05-21 23:42:44 -0500

    Automated venv initialization

--- run.bat
+++ run.bat
@@ -1,3 +1,22 @@
 @echo off
 
+if not exist .venv\ (
+	echo === Animal Crossing Map Viewer ===
+	echo Press any key to perform first-time virtual environment setup
+	pause >nul
+
+	echo Creating virtual environment, this may take a moment
+	python -m venv .venv >nul
+
+	echo Activating virtual environment
+	call .venv\Scripts\activate.bat
+
+	echo Installing dependencies
+	pip install -r requirements.txt >nul
+	echo Done!
+	echo(
+) else (
+	call .venv\Scripts\activate.bat
+)
+
 python main.py %~dp0 %~dp0assets\
